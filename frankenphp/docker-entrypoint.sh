a1e9f23ad01b9f94769ebab57a4d06d17ab74664
Florian <88294294+fouteox@users.noreply.github.com>
2024-02-16 21:54:06 +0100

    Initial commit

---
+++ frankenphp/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = 'frankenphp' ]; then
+	# Install the project the first time PHP is started
+	# After the installation, the following block can be deleted
+	if [ ! -f composer.json ]; then
+		rm -Rf tmp/
+		composer create-project "laravel/laravel $LARAVEL_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction
+
+		cd tmp
+		cp -Rp . ..
+		cd -
+		rm -Rf tmp/
+	fi
+
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX storage bootstrap/cache
+	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX storage bootstrap/cache
+fi
+
+exec docker-php-entrypoint "$@"
